d3761f9a4fa4eeba381641d08e58d6540fa37f4b
Akshaya Raghuthaman <akshayakoroth.ak@gmail.com>
2023-11-11 18:49:07 -0800

    add changes - getting started RoR

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,8 +1,27 @@
 class ArticlesController < ApplicationController
-    def new
+    def index
+        @articles = Article.all
     end
 
+    def new
+        @article = Article.new
+    end
+
+    # def create
+    #     render plain: params[:article].inspect
+    # end
     def create
-        render plain: params[:article].inspect
+        byebug
+        @article = Article.new(title: params[:article][:title], text: params[:article][:text])
+    
+        if @article.save
+          redirect_to @article
+        else
+          render :new, status: :unprocessable_entity
+        end
+    end
+
+    def show
+        @article = Article.find(params[:id])
     end
 end
