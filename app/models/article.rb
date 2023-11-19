2afa6eed6c93fbb8f817b97a8b218cd9cfb3d604
Akshaya Raghuthaman <akshayakoroth.ak@gmail.com>
2023-11-18 19:02:10 -0800

    Refactoring code by using concern

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,8 @@
 class Article < ApplicationRecord
+    include Visible
+
+    has_many :comments
+
     validates :title, presence: true
     validates :text, presence: true, length: { minimum: 10 }
-    has_many :comments
 end
